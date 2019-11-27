d1e79c7f10782ddd50a0975ec61b28963466d555
Aragon Nathan <nathan.aragon@etu.u-bordeaux.fr>
2019-11-27 09:48:56 +0100

    Adding Excepts on Keyboard Interupts

--- client.py
+++ client.py
@@ -9,71 +9,78 @@ from game import *
 
 
 def main():
-    serverAddress=""
     try:
-        serverAddress=sys.argv[1]
-    except Exception as e:
-        serverAddress=input("Adresse du serveur : ")
+        serverAddress=""
+        try:
+            serverAddress=sys.argv[1]
+        except:
+            serverAddress=input("Adresse du serveur : ")
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try :
-        server.connect((serverAddress, 7777))
-        print("Connecté ! En attente du deuxième joueur...")
-    except :
-        print("ERROR : Server unknown")
-        sys.exit(1)
-
-    #Recevoir son numero de joueur(0 ou 1)
-    playerNum = server.recv(16)
-    playerNum = int(playerNum.decode("utf-8"))
-    print("---LET THE GAME BEGIN---")
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try :
+            server.connect((serverAddress, 7777))
+            print("Connecté ! En attente du deuxième joueur...")
+        except :
+            print("ERROR : Server unknown")
+            sys.exit(1)
     
-    endConnection = False
-    while not endConnection:
-        #Recevoir le joueur actuel
-        currentPlayer = server.recv(16).decode("utf-8")
-        print("currentPlayer : ", currentPlayer)
-        currentPlayer = int(currentPlayer)
+        #Recevoir son numero de joueur(0 ou 1)
+        playerNum = server.recv(16)
+        playerNum = int(playerNum.decode("utf-8"))
+        print("---LET THE GAME BEGINS---")
+        
+        endConnection = False
+        while not endConnection:
+            #Recevoir le joueur actuel
+            currentPlayer = server.recv(16).decode("utf-8")
+            # print("currentPlayer : ", currentPlayer)
+            currentPlayer = int(currentPlayer)
 
-        #Recevoir la disposition de la partie si la partie n'est pas finie
-        if currentPlayer != -1:
-            (boats, shotsPlayer, shotsOther) = pickle.loads(server.recv(1024))
-            gameString = displayGame(boats, shotsPlayer, shotsOther)
-            print("======================")
-            print(gameString)
+            #Recevoir la disposition de la partie si la partie n'est pas finie
+            if currentPlayer != -1:
+                (boats, shotsPlayer, shotsOther) = pickle.loads(server.recv(1024))
+                gameString = displayGame(boats, shotsPlayer, shotsOther)
+                print("======================")
+                print(gameString)
 
-        if currentPlayer == playerNum:
-            msg = input("Quelle ColonneLigne ? ")
-            while not re.compile("[a-jA-J]{1}(10|[1-9]){1}$").match(msg):
-                #TEST valeur col valide
-                print("[Colonne][Ligne] ex : b" + str(random.randint(1,10)))
+            if currentPlayer == playerNum:
                 msg = input("Quelle ColonneLigne ? ")
-            server.send(msg.encode("utf-8"))
-        elif currentPlayer == -1:
-            #Fin de partie
-            print("Game over !")
-            time.sleep(0.1)
-            print("Ta grille : ")
-            #Réception des grilles des 2 joueurs
-            (myBoats, opponentBoats, shotsPlayer, shotsOther) = pickle.loads(server.recv(1024))
+                while not re.compile("[a-jA-J]{1}(10|[1-9]){1}$").match(msg):
+                    #TEST valeur col valide
+                    print("[Colonne][Ligne] ex : b" + str(random.randint(1,10)))
+                    msg = input("Quelle ColonneLigne ? ")
+                server.send(msg.encode("utf-8"))
+            elif currentPlayer == -1:
+                #Fin de partie
+                print("Game over !")
+                time.sleep(0.1)
+                print("Ta grille : ")
+                #Réception des grilles des 2 joueurs
+                (myBoats, opponentBoats, shotsPlayer, shotsOther) = pickle.loads(server.recv(1024))
 
-            myGrid = displayConfiguration(boats, shotsOther, showBoats=True)
-            print(myGrid)
-            time.sleep(0.1)
-            opponentGrid = displayConfiguration(opponentBoats, shotsPlayer, showBoats=True)
-            print("La grille de l'adversaire : ")
-            print(opponentGrid)
-            #Réception du gagnant
-            winner = server.recv(16).decode("utf-8")
-            winner = (int(winner))
+                myGrid = displayConfiguration(boats, shotsOther, showBoats=True)
+                print(myGrid)
+                time.sleep(0.1)
+                opponentGrid = displayConfiguration(opponentBoats, shotsPlayer, showBoats=True)
+                print("La grille de l'adversaire : ")
+                print(opponentGrid)
+                #Réception du gagnant
+                winner = server.recv(16).decode("utf-8")
+                winner = (int(winner))
 
-            if winner == playerNum:
-                print("Wouhou tu as gagné")
+                if winner == playerNum:
+                    print("Wouhou tu as gagné")
+                else:
+                    print("C'est perdu")
+                endConnection = True
             else:
-                print("C'est perdu")
-            endConnection = True
-        else:
-            print("L'autre joueur joue...")
+                print("L'autre joueur joue...")
+    except KeyboardInterrupt:
+        print("\nEndig game...")
+        sys.exit(1)
+    except ValueError:
+        print("The other player disconnect, YOU WON")
+        sys.exit(0)
 
 
 """ display the game viewer by the player"""
--- server.py
+++ server.py
@@ -39,89 +39,96 @@ def sendGameOver(sJ0, sJ1, game):
     time.sleep(0.1)
     s.close()
 
-
-
 def main():
-    s = None
-    clients = None
-    game = None
-    sJ0 = None
-    sJ1 = None
-    game = Game(randomConfiguration(), randomConfiguration())
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', 7777))
-    s.listen(1)
-    clients = [s]
-    waitingPlayers = True
-    colDemandee = False
-    ligneDemandee = False
-    inGame=True
-    print("En attente de clients...")
-    while inGame:
-        read1, write1, error1 = select.select(clients, [], [])
-        for sk in read1:
-            if sk == s:  #Connection
-                s2, addr = s.accept()
-                clients.append(s2)
-                print("Connection of the address " + addr[0])
-                if len(clients) > 2:
-                    #Deux joueurs sont connectés : début de la partie
-                    waitingPlayers = False
+    try:
+        s = None
+        clients = None
+        game = None
+        sJ0 = None
+        sJ1 = None
+        game = Game(randomConfiguration(), randomConfiguration())
+        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('', 7777))
+        s.listen(1)
+        clients = [s]
+        waitingPlayers = True
+        colDemandee = False
+        ligneDemandee = False
+        inGame=True
+        print("En attente de clients...")
+        while inGame:
+            read1, write1, error1 = select.select(clients, [], [])
+            for sk in read1:
+                if sk == s:  #Connection
+                    s2, addr = s.accept()
+                    clients.append(s2)
+                    print("Connection of the address " + addr[0])
+                    if len(clients) > 2:
+                        #Deux joueurs sont connectés : début de la partie
+                        waitingPlayers = False
+                        time.sleep(0.1)
+                        print("2 joueurs connectés : \"Let the game begins !\"")
+                        sJ0 = clients[1]
+                        sJ1 = clients[2]
+                        #Envoyer num joueur au deuxième chaque joueur
+                        sJ0.send("0".encode("utf-8"))
+                        sJ1.send("1".encode("utf-8"))
+                        time.sleep(0.1)
+                        #Envoyer le currentPlayer à chaque joueur
+                        sJ0.send("0".encode("utf-8"))
+                        sJ1.send("0".encode("utf-8"))
+                        time.sleep(0.1)
+                        #Envoyer les tableaux de dispositions
+                        sJ0.send(pickle.dumps((game.boats[0], game.shots[0], game.shots[1])))
+                        sJ1.send(pickle.dumps((game.boats[1], game.shots[1], game.shots[0])))
+
+                        currentPlayer = 0
+                else:  #Commands reception
+                    line = sk.recv(512).decode("utf-8")
+                    if line.encode("utf-8") == b'':
+                        for sock in clients:
+                            print("Disconnection of the address " + sock.getsockname()[0])
+                            sock.close()
+                            # clients.remove(sock)
+                        inGame=False
+                        print("-----------GAME ENDED-----------")
+                        break
+
+                    print("J" + str(currentPlayer+1) + " : " + line)
+                    mots = line.split()
+                    rep = line.replace('\n','')
+
+                    x = ord(rep[0].capitalize())-ord("A")+1
+                    y = int(rep[1])
+                    addShot(game, x, y, currentPlayer)
+                    if gameOver(game) != -1:
+                        sendGameOver(sJ0, sJ1, game)
+                        inGame=False
+                        time.sleep(0.1)
+
+
+                    if currentPlayer == J0 and sk == sJ0:
+                        sJ0.send("1".encode("utf-8"))
+                        sJ1.send("1".encode("utf-8"))
+                    elif currentPlayer == J1 and sk == sJ1:
+                        sJ0.send("0".encode("utf-8"))
+                        sJ1.send("0".encode("utf-8"))
+                    else:
+                        print("WRONG")
+                        sys.exit(1)
+
                     time.sleep(0.1)
-                    print("2 joueurs connectés : \"Let the game begins !\"")
-                    sJ0 = clients[1]
-                    sJ1 = clients[2]
-                    #Envoyer num joueur au deuxième chaque joueur
-                    sJ0.send("0".encode("utf-8"))
-                    sJ1.send("1".encode("utf-8"))
-                    time.sleep(0.1)
-                    #Envoyer le currentPlayer à chaque joueur
-                    sJ0.send("0".encode("utf-8"))
-                    sJ1.send("0".encode("utf-8"))
-                    time.sleep(0.1)
-                    #Envoyer les tableaux de dispositions
-                    sJ0.send(pickle.dumps((game.boats[0], game.shots[0], game.shots[1])))
-                    sJ1.send(pickle.dumps((game.boats[1], game.shots[1], game.shots[0])))
 
-                    currentPlayer = 0
-            else:  #Commands reception
-                line = sk.recv(512).decode("utf-8")
-                print("J" + str(currentPlayer+1) + " : " + line)
-                mots = line.split()
-                rep = line.replace('\n','')
-                if line.encode("utf-8") == b'':
-                    print("Disconnection of the address " + sk.getsockname()[0])
-                    sk.close()
-                    clients.remove(sk)
-
-                x = ord(rep[0].capitalize())-ord("A")+1
-                y = int(rep[1])
-                addShot(game, x, y, currentPlayer)
-                if gameOver(game) != -1:
-                    sendGameOver(sJ0, sJ1, game)
-                    inGame=False
-                    time.sleep(0.1)
-
-
-                if currentPlayer == J0 and sk == sJ0:
-                    sJ0.send("1".encode("utf-8"))
-                    sJ1.send("1".encode("utf-8"))
-                elif currentPlayer == J1 and sk == sJ1:
-                    sJ0.send("0".encode("utf-8"))
-                    sJ1.send("0".encode("utf-8"))
-                else:
-                    print("WRONG")
-                    sys.exit(1)
-
-                time.sleep(0.1)
-
-                if inGame:
-                    sJ0.send(pickle.dumps((game.boats[0], game.shots[0], game.shots[1])))
-                    sJ1.send(pickle.dumps((game.boats[1], game.shots[1], game.shots[0])))
-                    currentPlayer = (currentPlayer+1)%2
+                    if inGame:
+                        sJ0.send(pickle.dumps((game.boats[0], game.shots[0], game.shots[1])))
+                        sJ1.send(pickle.dumps((game.boats[1], game.shots[1], game.shots[0])))
+                        currentPlayer = (currentPlayer+1)%2
+    except KeyboardInterrupt:
+        print("\nEnding Server...")
+        sys.exit(0)
 
 print("Lancement du Serveur...")
 while True:
-    print("---------NOUVELLE PARTIE-------")
+    print("-----------NEW GAME-----------")
     main()
